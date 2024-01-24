0f4225472cbde39d8ab1b3c2f5f5e4242d3b6f3c
Aloé, Noah <noah.aloe@comline.de>
2024-01-24 14:20:57 +0100

    Testatkarte 4 hinzugefügt

---
+++ Testat4/4/4.ino
@@ -0,0 +1,57 @@
+int trigger = 7;
+int echo = 8;
+int soundPin = 10;
+
+// Schallgeschwindigkeit in der Luft (in cm/ms)
+// wird benötigt, um die Laufzeit in eine Entfernung
+float speedOfSound = 0.03432;
+
+void setup() {
+    pinMode(echo, INPUT);
+    pinMode(trigger, OUTPUT);
+    pinMode(soundPin, OUTPUT);
+    Serial.begin(9600);
+}
+
+void loop() {
+
+    digitalWrite(trigger, LOW);
+    delay(5);
+
+    digitalWrite(trigger, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigger, LOW);
+
+    long time = pulseIn(echo, HIGH);
+
+    long distance = (time / 2) * speedOfSound;
+
+    Serial.print("cm: ");
+    Serial.println(distance);
+
+    lamp(distance);
+
+    delay(500);
+}
+
+void lamp(long distance) {
+    int mode = HIGH;
+    for (int = 0; i < distance; i++) {
+        digitalWrite(soundPin, mode);
+        delay(10);
+        if (mode == HIGH) {
+            mode = LOW;
+        } else {
+            mode = HIGH;
+        }
+    }
+
+}
+
+void sound(long distance) {
+    if (distance == 0 || distance >= 15) {
+        noTone(soundPin);
+    } else {
+        tone(soundPin, distance * 100);
+    }
+}
